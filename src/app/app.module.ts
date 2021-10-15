abf36e2afe1e982534a5a3ca7b5c53e89de8fee6
Luis Pedro Morales <luismorarod98@gmail.com>
2021-10-14 19:58:35 -0600

    update generated pdf file name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { CinemasManagementComponent } from './cinemas-management/cinemas-managem
 import { ScreeningManagementComponent } from './screening-management/screening-management.component';
 import { TheatersManagementComponent } from './theaters-management/theaters-management.component';
 import { AdminNavbarComponent } from './admin-navbar/admin-navbar.component';
+import { InvoiceComponent } from './invoice/invoice.component';
 
 const appRoutes : Routes = [
   { path: '' , component : LoginComponent},
@@ -27,7 +28,8 @@ const appRoutes : Routes = [
   { path: 'theaters' , component : TheatersManagementComponent},
   { path: 'screening' , component : ScreeningManagementComponent},
   { path: 'screening' , component : ScreeningManagementComponent},
-  { path: 'register/client' , component : ClientFormComponent}
+  { path: 'register/client' , component : ClientFormComponent},
+  { path: 'invoice', component: InvoiceComponent}
 ]
 @NgModule({
   declarations: [
@@ -44,7 +46,8 @@ const appRoutes : Routes = [
     MoviesManagementComponent,
     ScreeningManagementComponent,
     CinemasManagementComponent,
-    TheatersManagementComponent
+    TheatersManagementComponent,
+    InvoiceComponent
   ],
   imports: [
     BrowserModule,
