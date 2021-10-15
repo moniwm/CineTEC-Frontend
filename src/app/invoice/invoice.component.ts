f8dc1ce9a8c05e1eb992d003f70bde8cb0ddba2c
Luis Pedro Morales <luismorarod98@gmail.com>
2021-10-14 22:51:42 -0600

    add InvoiceComponent class attributes

--- src/app/invoice/invoice.component.ts
+++ src/app/invoice/invoice.component.ts
@@ -9,22 +9,46 @@ import jsPDFInvoiceTemplate, { OutputType, jsPDF } from "jspdf-invoice-template"
   providers: [DataService]
 })
 export class InvoiceComponent implements OnInit {
+  invoiceNum: number;
+  clientName: string;
+  clientLName: string;
+  phoneNum: string;
+  movie: string;
+  screening: string;
+  theater: string;
+  price: number;
+  quantity: number;
+  total: number;
+  subtotal: number;
 
-  constructor(private dataSvc: DataService) { }
+
+  constructor(private dataSvc: DataService) {
+     this.invoiceNum = 0;
+     this.clientName = "Luis Pedro";
+     this.clientLName = "Morales"
+     this.phoneNum = "61538981";
+     this.movie = "Venom";
+     this.screening = "16:00"
+     this.theater = "Paseo";
+     this.price = 3500 / 1.13;
+     this.quantity = 2;
+     this.total = this.quantity * +this.price;
+     this.subtotal = this.total * 1.13;
+   }
 
   ngOnInit(): void {
   }
 
   public createInvoice(): void {
-    const invoiceNum: Number = 0;
+    
 
     var props = {
       outputType: OutputType.Save,
       returnJsPDFDocObject: true,
-      fileName: "CineTEC_purchase" + invoiceNum.toString(),
+      fileName: "CineTEC_purchase" + this.invoiceNum.toString(),
       orientationLandscape: false,
       logo: {
-        src: "https://raw.githubusercontent.com/edisonneza/jspdf-invoice-template/demo/images/logo.png",
+        src: "../assets/logo.jpeg",
         width: 53.33, //aspect ratio = width/height
         height: 26.66,
         margin: {
@@ -33,58 +57,53 @@ export class InvoiceComponent implements OnInit {
         }
       },
       business: {
-        name: "Business Name",
-        address: "Albania, Tirane ish-Dogana, Durres 2001",
-        phone: "(+355) 069 11 11 111",
-        email: "email@example.com",
-        email_1: "info@example.al",
-        website: "www.example.al",
+        name: "CineTEC",
+        address: "Barrio Escalante, Avenida 7, San José, Costa Rica",
+        phone: "(+506) 2555 5555",
+        email: "costumer_service@cinetec.com",
+        website: "www.cinetec.cr",
       },
       contact: {
         label: "Invoice issued for:",
-        name: "Client Name",
-        address: "Albania, Tirane, Astir",
-        phone: "(+355) 069 22 22 222",
-        email: "client@website.al",
-        otherInfo: "www.website.al",
+        name: this.clientName + ' ' + this.clientLName,
+        phone: '(+506) ' + this.phoneNum,
+        email: this.clientName.charAt(0).toLowerCase() + this.clientLName.toLowerCase() + "@gmail.com",
       },
       invoice: {
-        label: "Invoice #: ",
-        num: 19,
+        label: "Invoice number: ",
+        num: this.invoiceNum,
         invDate: "Payment Date: 01/01/2021 18:12",
         invGenDate: "Invoice Date: 02/02/2021 10:17",
         headerBorder: false,
         tableBodyBorder: false,
-        header: ["#", "Description", "Price", "Quantity", "Unit", "Total"],
-        table: Array.from(Array(10), (item, index) => ([
+        header: ["Purchase", "Movie", "Screening", "Theater", "Price", "Quantity", "Total"],
+        table: Array.from(Array(1), (item, index) => ([
           index + 1,
-          "There are many variations ",
-          200.5,
-          4.5,
-          "m2",
-          400.5
+          this.movie,
+          this.screening,
+          this.theater,
+          Math.floor(this.price),
+          this.quantity, 
+          Math.floor(this.total)
         ])),
         invTotalLabel: "Total:",
-        invTotal: "145,250.50",
-        invCurrency: "ALL",
+        invTotal: Math.floor(this.total).toString() + " \u00A2",
         row1: {
-          col1: 'VAT:',
-          col2: '20',
-          col3: '%',
+          col1: 'IVA:',
+          col2: '13%',
           style: {
             fontSize: 10 //optional, default 12
           }
         },
         row2: {
           col1: 'SubTotal:',
-          col2: '116,199.90',
-          col3: 'ALL',
+          col2: this.subtotal.toString() + " \u00A2",
           style: {
             fontSize: 10 //optional, default 12
           }
-        },
-        invDescLabel: "Invoice Note",
-        invDesc: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.",
+        }
+        //invDescLabel: "Invoice Note",
+        //invDesc: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.",
       },
       footer: {
         text: "The invoice is created on a computer and is valid without the signature and stamp.",
